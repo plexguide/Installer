0c365ca6bd0584f5506c17aa6e65b85af97cbec2
Admin9705 <9705@duck.com>
2024-08-28 23:29:54 -0400

    update

--- menu_exit.sh
+++ menu_exit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # ANSI color codes
-RED="\033[0;31m"
+BRIGHT_RED="\033[1;31m"
 ORANGE="\033[0;33m"
+YELLOW="\033[1;33m"
+LIGHT_GREEN="\033[1;32m"
+CYAN="\033[0;36m"
 PURPLE="\033[0;35m"
-LIGHT_BLUE="\033[1;34m"
 NC="\033[0m" # No color
 
 # Clear the screen at the start
@@ -14,11 +16,12 @@ clear
 echo "Visit github.com/plexguide/PlexGuide.com or plexguide.com"
 echo ""  # Space for separation
 echo "Commands:"
-echo -e "[${ORANGE}1${NC}] plexguide   |  Deploy PlexGuide"
+echo -e "[${BRIGHT_RED}1${NC}] plexguide   |  Deploy PlexGuide"
 echo -e "[${ORANGE}2${NC}] pg          |  Deploy PlexGuide"
-echo -e "[${RED}3${NC}] pgalpha     |  Install Latest Alpha Build"
-echo -e "[${PURPLE}4${NC}] pgbeta      |  Install Latest Beta Build"
-echo -e "[${LIGHT_BLUE}5${NC}] pgfork      |  Deploy PGFork"  # New command for PGFork in light blue
+echo -e "[${YELLOW}3${NC}] pgalpha     |  Install Latest Alpha Build"
+echo -e "[${LIGHT_GREEN}4${NC}] pgbeta      |  Install Latest Beta Build"
+echo -e "[${CYAN}5${NC}] pgfork      |  Deploy PGFork"
+echo -e "[${PURPLE}6${NC}] pginstall   |  Access the Installer Menu (Helps Repairs)"  # New command for Installer Menu
 echo ""  # Space before exiting
 
 # Exit the script
