515207ee56736633f401f715a598cd81c159b9b4
Admin9705 <9705@duck.com>
2024-09-13 14:24:57 -0400

    update

--- menu_exit.sh
+++ menu_exit.sh
@@ -23,9 +23,9 @@ echo ""  # Space for separation
 echo -e "${BOLD}Commands:${NC}"
 echo -e "[${BRIGHT_RED}1${NC}] plexguide   |  Deploy PlexGuide"
 echo -e "[${ORANGE}2${NC}] pg          |  Deploy PlexGuide"
-echo -e "[${LIGHT_GREEN}3${NC}] pgstable    |  Install Latest Dev Build"
+echo -e "[${LIGHT_GREEN}3${NC}] pgstable    |  Install Latest Stable Build"
+echo -e "[${YELLOW}4${NC}] pgbeta      |  Install Latest Beta Build"
 echo -e "[${YELLOW}3${NC}] pgdev       |  Install Latest Dev Build"
-echo -e "[${LIGHT_GREEN}4${NC}] pgbeta      |  Install Latest Beta Build"
 echo -e "[${CYAN}5${NC}] pgfork      |  Fork PlexGuide"
 echo -e "[${PURPLE}6${NC}] pginstall   |  Access the Installer Menu (Helps Repairs)"
 echo ""  # Space before exiting
