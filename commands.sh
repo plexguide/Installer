3a5de05f81cd45373a0883791fe1ad2acc67b263
Admin9705 <9705@duck.com>
2024-09-17 05:14:09 -0400

    update

--- commands.sh
+++ commands.sh
@@ -75,40 +75,12 @@ create_command_symlinks() {
     info "Command symlinks created successfully."
 }
 
-# Function to set up the pginstall command
-setup_pginstall_command() {
-    #info "Setting up pginstall command..."
-
-    # Define the URL of the install script
-    #local install_script_url="https://raw.githubusercontent.com/plexguide/Installer/v11/install_menu.sh"
-
-    # Define the directory and script name for temporary storage
-    ##local tmp_dir="/pg/tmp"
-    #local tmp_script="$tmp_dir/install_menu_tmp.sh"
-
-    # Ensure the temporary directory exists
-    #sudo mkdir -p "$tmp_dir"
-
-    # Write the pginstall script that will download the install script to the tmp directory and execute it
-    #cat << EOF | sudo tee /usr/local/bin/pginstall > /dev/null
-#!/bin/bash
-#echo "Downloading and executing the PG installer..."
-
-# Download the installation script
-#curl -sL "$install_script_url" -o "$tmp_script"
-
-# Set the script as executable
-#chmod +x "$tmp_script"
-
-# Execute the script
-#bash "$tmp_script"
-#EOF
-
-#    # Set ownership and permissions for the pginstall script
-#    sudo chown 1000:1000 /usr/local/bin/pginstall
-#    sudo chmod 755 /usr/local/bin/pginstall
-
-#    info "pginstall command setup complete. You can now use the pginstall command to run the installer."
+# Build PG Temp Directory
+setup_temp() {
+    local tmp_dir="/pg/tmp"
+    sudo mkdir -p "$tmp_dir"
+    chmod +x "$tmp_script"
+    bash "$tmp_script"
 }
 
 # Function to ensure all created commands are 1000:1000 and executable
@@ -147,7 +119,7 @@ update_pg_permissions() {
 # Main script execution
 execute_adduser_script
 create_command_symlinks
-#setup_pginstall_command
+setup_temp
 ensure_command_permissions
 update_pg_permissions
 
